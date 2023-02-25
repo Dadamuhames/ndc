b012616f25318199bc29d19bbadf2d8cd3f5eab7
MSD <dadamuhames@gmail.com>
2023-02-25 13:45:51 +0500

    first

--- ndc/settings.py
+++ ndc/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages', 'django.contrib.staticfiles',
     'main', 'corsheaders', 'rest_framework',
-    'easy_thumbnails', 'admins', 'autoslug',
+    'easy_thumbnails', 'admins',
     'django_cleanup.apps.CleanupConfig',
 ]
 
